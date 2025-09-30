b2597e732a6a18901573cdb9f3d274ec8cbb823e
Edgar-Moises-L <moisesleyva202@gmail.com>
2025-09-30 13:07:24 -0700

    modificacion en seguridad

--- sushi/src/main/java/com/konoha/sushi/permission/PermissionRepository.java
+++ sushi/src/main/java/com/konoha/sushi/permission/PermissionRepository.java
@@ -2,11 +2,12 @@ package com.konoha.sushi.permission;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@RestController
+@Repository
 public interface PermissionRepository extends JpaRepository<PermissionEntity, Long > {
     Optional<PermissionEntity> findByName(String name);
 }
